199924ce34c808407b0c692fb0b8b0883d6970ac
darafsa <marek1999@online.de>
2022-07-26 00:03:35 +0200

    Home Content
    * added Kenjutsu description from wikipedia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,54 @@
 <template>
-	<div>
+	<div id="home-container">
 		<h1>KENJUTSU</h1>
-		<h2>Jena</h2>
-		<div class="test"></div>
+		<div class="box">
+			<p>
+				<b>Kenjutsu</b> (japanisch 剣術 &bdquo;Schwerttechnik(en),
+				Schwertkunst&ldquo;) ist der Oberbegriff aller Formen der
+				japanischen <b>Schwertkunst</b>, insbesondere jener Disziplinen,
+				welche vor der Meiji-Zeit entstanden. Die heutigen Formen des
+				modernen Kendō und Iaidō, die im 20. Jahrhundert entstanden, haben
+				ebenfalls ihren Ursprung zu dieser Zeit. Praktizierende des Kenjutsu
+				heißen Kenshi (剣士).
+			</p>
+			<p>
+				<i>Kenjutsu</i> in all seinen ursprünglichen Ausprägungen wurde von
+				den feudalen Samurai (früher Bushi), der japanischen Krieger, als
+				Disziplinen der Waffenführung begründet. Die Bedeutung des Wortes
+				Kenjutsu kann mit &bdquo;Methode oder Technik zur Führung des
+				Schwertes&ldquo; übersetzt werden, wobei <i>jutsu</i> sich dezidiert
+				auf die technische Anwendung bezieht, im Gegensatz zur Endung
+				<i>Dō</i> - wie beispielsweise in Kendō, Iaidō, Judō usw. - die eine
+				Kunstfertigkeit mit geistigem Inhalt bezeichnet.
+			</p>
+			<p>
+				Da Kenjutsu eine ganze Gruppe von Disziplinen mit Handhabung des
+				Schwertes umschreibt, sind die technischen Anwendungen und die ihnen
+				zugrundeliegenden Instrumente, Unterrichtsformen sowie Strategien
+				und Taktiken auf dem Schlachtfeld oftmals ebenso grundverschieden.
+				In einigen Disziplinen wurden hölzerne Nachbildungen des Schwertes
+				verwendet (Bokken, auch Bokutō) und mit Körpertreffern im
+				Vollkontakt geübt. Andere Formen unterrichteten gänzlich ohne
+				Übungspartner und verwendeten dafür mehrheitlich Kata - stilisierte
+				Übungsformen. Da unter der Endung -<i>jutsu</i> alle technischen
+				Anwendungen mit dem Schwert gemeint sind, fallen auch ad hoc und
+				intuitiv entstandene Formen darunter, denn letztendliche Zielsetzung
+				war die Überlegenheit auf dem Schlachtfeld und dienlich dazu war
+				auch jeder intuitive taktische Überraschungsmoment.
+			</p>
+			<p><i>Quelle: <a href="https://de.wikipedia.org/wiki/Kenjutsu" target="_blank">Wikipedia</a></i></p>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style lang="scss" scoped>
-
-.test {
-	height: 100rem;
+<style lang="scss">
+#home-container {
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
 }
 </style>
