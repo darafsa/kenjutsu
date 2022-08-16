ba1d3e7e56d9617394865ac79f874b7c25b82296
darafsa <marek1999@online.de>
2022-08-16 10:49:47 +0200

    About Content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,14 +1,55 @@
 <template>
-	<div>
+	<div id="about-container">
 		<h1>KENJUTSU in Jena</h1>
-		
+		<div class="box">
+			<p>
+				Kenjutsu in Jena wird über den
+				<a
+					href="https://www.hochschulsportbuchung.uni-jena.de/angebote/aktueller_zeitraum/_Ken_Jutsu.html"
+					target="_blank"
+					><i>Hochschulsport</i></a
+				>
+				der Friedrich Schiller Universität Jena angeboten.
+			</p>
+			<h2>Training</h2>
+			<p>
+				Für das Training reicht zu Beginn bequeme Sportbekleidung. Ein
+				<i>Bokken</i> ist für den Anfang nicht zwingend notwendig, für den
+				weiteren Verlauf des Kurses ist ein eigenes hölzernes Übungsschwert
+				allerdings von Vorteil, Kostenpunkt ca. 15 Euro.
+			</p>
+			<p>
+				Das Dojo ist mit Matten ausgelegt und darf deshalb nur barfuß
+				betreten werden. Da die Schuhe am Eingang zu wechseln sind, benötigt
+				man <b>Badelatschen</b> oder ähnliches, um vom Eingang über die
+				Umkleidekabinen zur Matte zu gelangen. Barfuß oder auf Socken zu
+				laufen ist aus hygienischen Gründen nicht möglich.
+			</p>
+			<h2>Zeit und Ort</h2>
+			<p>
+				Freitags 19:30 - 21:00<br />
+				Oberaue 1, 07743 Jena [<a
+					href="https://goo.gl/maps/CYNNGCRiPk1shwFE6"
+					target="_blank"
+					><i>Maps</i></a
+				>]
+			</p>
+			<p>
+				Ein Schnuppertraining ist grundsätzlich <b>immer</b> Möglich,
+				vorzugsweise aber zu Beginn eines jeden Semesters.
+			</p>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+#about-container {
+	width: 100vw;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
 </style>
